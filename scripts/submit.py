62aba43048d25c117d8fd240f87ece2ecbf64916
nedmv <nedmvl@rambler.ru>
2024-06-07 23:53:18 +0300

    refactor(scripts): remove redundant code

--- scripts/submit.py
+++ scripts/submit.py
@@ -3,28 +3,6 @@ import argparse
 import code_weekend as cw
 from get_submission import process_submission
 
-def init_submission_dir(submission_id):
-    path = os.path.join("submissions")
-    if not os.path.exists(path):
-        os.mkdir(path)
-    path = os.path.join(path,f"{submission_id}")
-    if not os.path.exists(path):
-        os.mkdir(path)
-
-def save_info(submission_id, submission_info):
-    path = os.path.join("submissions", f"{submission_id}", "info")
-    if not os.path.isfile(path):
-        with open(path, 'wb') as f:
-            f.write(submission_info)
-    print(f"Submission {submission_id}'s result is available at {path}")
-
-def save_solution(submission_id, solution):
-    path = os.path.join("submissions", f"{submission_id}", "solution")
-    if not os.path.isfile(path):
-        with open(path, 'w') as f:
-            f.write(solution)
-    print(f"Submission {submission_id}'s solution is available at {path}")        
-
 def main():
     parser = argparse.ArgumentParser("Submit a solution for a Code Weekend task.")
     parser.add_argument('task', type=int, nargs=1, help='id of the task to submit')
