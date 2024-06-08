26d2acd373929c95e7ff3195703634242326da94
nedmv <nedmvl@rambler.ru>
2024-06-08 03:47:10 +0300

    feat(scripts): add solve.py

---
+++ scripts/solve.py
@@ -0,0 +1,51 @@
+import os
+import argparse
+import subprocess
+import code_weekend as cw
+from submit import submit
+
+
+def solve(path: str, task_id: int, solver_id: int, send = False) -> str:
+  #TODO: write to file here and do not download from site
+  #base = os.path.basename(path)
+  #solution_path = os.path.join("solutions", f"{task_id}", f"{base}", f"{solver_id}")
+  input_path = os.path.join("tasks", f"{task_id}", "input")
+  output_path = os.path.join("tasks", f"{task_id}", "last_output")
+
+  result = subprocess.run([f'{path}', '-i', f'{input_path}', '-o', f'{output_path}', '-s', f'{solver_id}'], capture_output=True)
+  if result.returncode != 0:
+    print(f"Failed to solve task {task_id} with solver {path} {solver_id}")
+    print(result.stdout)
+    print(result.stderr)
+    return
+
+  solution = None
+  with open(output_path, 'r') as f:
+    solution = f.read()
+
+  if send and solution is not None:
+    submit(task_id, solution, wait=True, save=True)
+  print(f"Solution for task {task_id} with solver {path} {solver_id}")
+  print(solution)
+
+def main():
+    parser = argparse.ArgumentParser("Solve task with the given solver.")
+    # parser.add_argument('task', type=int, nargs=1, help='id of the task to solve')
+    parser.add_argument('task', type=int, nargs=2, help='task range (inclusive)')
+    parser.add_argument('solver', type=str, nargs=1, help='path to the solver')
+    parser.add_argument('-i', '--id', type=int, default=0, help='internal id of the solver')
+    parser.add_argument('-s', '--submit', action='store_true', default=False, help='submit solution')
+    args = parser.parse_args()
+
+    min_id = min(args.task[0], args.task[1])
+    max_id = max(args.task[0], args.task[1])
+
+    solver = args.solver[0]
+
+    for task_id in range(min_id, max_id+1):
+      solution = solve(solver, task_id, args.id, args.submit)
+      if args.submit:
+        submit(task_id, solution, wait=True, save=True)
+
+if __name__ == "__main__":
+    main()
--- scripts/submit.py
+++ scripts/submit.py
@@ -3,6 +3,16 @@ import argparse
 import code_weekend as cw
 from get_submission import process_submission
 
+def submit(task_id: int, solution: str, wait = False, save = False):
+    submission_id = cw.submit(task_id, solution)
+    if submission_id is None:
+        print(f'Failed to send submission for task {task_id}.')
+        return
+    print(f'Submission {submission_id} for task {task_id} is sent.')
+    info = cw.get_submission_info(submission_id, wait=True)
+    print(f'Result for submission {submission_id}: {info}')
+    process_submission(submission_id, wait, save)
+
 def main():
     parser = argparse.ArgumentParser("Submit a solution for a Code Weekend task.")
     parser.add_argument('task', type=int, nargs=1, help='id of the task to submit')
@@ -19,15 +29,7 @@ def main():
     else:
         solution = args.solution[0]
 
-    submission_id = cw.submit(task_id, solution)
-    if submission_id is None:
-        print(f'Failed to send submission for task {task_id}.')
-        return
-    print(f'Submission {submission_id} for task {task_id} is sent.')
-    info = cw.get_submission_info(submission_id, wait=True)
-    print(f'Result for submission {submission_id}: {info}')
-
-    process_submission(submission_id, wait=True, save_solution=args.save)
+    submit(task_id, solution, args.save)
 
 if __name__ == "__main__":
     main()
